6449b78960e10306083d0e78f6d4f78dcd625573
dennistech <33478452+dennistech@users.noreply.github.com>
2019-01-17 14:51:04 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4cab0a4359ce778b0f871187318cbf1a.js"
+  "/portfolio/precache-manifest.7d88ef57bc8a49d698a894002cb48d85.js"
 );
 
 workbox.clientsClaim();
