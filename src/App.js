39a9f99b363f582b18fabc81f1d4e067d0246698
dennistech <33478452+dennistech@users.noreply.github.com>
2019-01-17 14:16:34 -0800

    create HOME page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,71 @@
 import React, { Component } from 'react';
 import './App.css';
+import './css/main.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showMenu: false,
+    };
+  }
+
+  toggleMenu = () => {
+    this.setState({ showMenu: !this.state.showMenu });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>My Portfolio</h1>
+        <header>
+          <div className={`menu-btn ${this.state.showMenu ? 'close' : '' }`} onClick={this.toggleMenu}>
+            <div className="btn-line" />
+            <div className="btn-line" />
+            <div className="btn-line" />
+          </div>
+
+          <nav className={`menu ${this.state.showMenu ? 'show' : '' }`}>
+            <ul className={`menu-nav ${this.state.showMenu ? 'show' : '' }`}>
+              <li className={`nav-item current ${this.state.showMenu ? 'show' : '' }`}>
+                <a href="/" className="nav-link">
+                  Home
+                </a>
+              </li>
+              <li className={`nav-item ${this.state.showMenu ? 'show' : '' }`}>
+                <a href="/about" className="nav-link">
+                  About Me
+                </a>
+              </li>
+              <li className={`nav-item ${this.state.showMenu ? 'show' : '' }`}>
+                <a href="/projects" className="nav-link">
+                  Projects
+                </a>
+              </li>
+              <li className={`nav-item ${this.state.showMenu ? 'show' : '' }`}>
+                <a href="/contact" className="nav-link">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main id="home">
+          <h1 className="lg-heading">
+            Dennis <span className="text-secondary">Mok</span>
+          </h1>
+          <h2 className="sm-heading">
+            Front End Software Engineer, Developer & Programmer
+          </h2>
+          <div className="icons">
+            <a href="#!">
+              <i className="fab fa-linkedin fa-2x" />
+            </a>
+            <a href="#!">
+              <i className="fab fa-github fa-2x" />
+            </a>
+          </div>
+        </main>
       </div>
     );
   }
